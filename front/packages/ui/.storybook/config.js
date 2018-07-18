80a2af908790b88d69118aadfdeaa9421d278731
Amine SADRY <asadry.ext@louvre-hotels.com>
2018-07-18 09:46:40 +0200

    Config storybook Config storybook to use .story.js files from src folder

--- front/packages/ui/.storybook/config.js
+++ front/packages/ui/.storybook/config.js
@@ -1,9 +1,20 @@
 import { configure } from '@storybook/react';
 
-// automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.js$/);
-function loadStories() {
-  req.keys().forEach(filename => req(filename));
+//automatically import all files ending in *.stories.js
+// const req = require.context('../stories', true, /.stories.js$/);
+
+// function loadStories() {
+//   req.keys().forEach(filename => req(filename));
+// }
+
+
+function requireAll(requireContext) {
+  return requireContext.keys().map(requireContext);
 }
+
+function loadStories() {
+  requireAll(require.context("../src", true, /.story\.js?$/));
+}
+
 
 configure(loadStories, module);
